3e911fec24c4732779a033d5c32f610a1014d0ad
KarthikPalani-devops <59925065+KarthikPalani-devops@users.noreply.github.com>
2021-03-19 15:34:12 +0530

    Add files via upload

---
+++ exp3.rb
@@ -0,0 +1,23 @@
+puts "I will now count my chickens"
+
+puts "hens #{25+30/6}"
+puts "Rooster #{100-25*3%4}"
+
+puts "Now I will count the eggs:"
+
+puts 3+2+1-5+4%2-1/4+6
+
+puts "Is it tru that 3 + 2 < 5 - 7?"
+puts 3+2<5-7
+
+
+puts "what is 3+2? #{3+2}"
+puts "what is 5-7? #{5-7}"
+
+puts "oh that's why its False"
+
+puts "how about sme more"
+
+puts "is it greater? #{5>-2}"
+puts "is it greater or equal? #{5>=-2}"
+puts "is less or eqaul? #{5<=-2}"
