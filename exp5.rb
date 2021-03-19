e4da00598bf1a1a8df7e62153dd023865a0ec2c8
KarthikPalani-devops <59925065+KarthikPalani-devops@users.noreply.github.com>
2021-03-19 17:02:24 +0530

    Add files via upload

---
+++ exp5.rb
@@ -0,0 +1,18 @@
+name = 'karthik'
+age = 25
+height = 178 # in centimeters
+weight = 65 #in Kg
+eyes = 'balck'
+teeth = 'white'
+hair = 'brown'
+one_inch = 0.393701  #centimeters
+one_pound = 2.20462  #in KG's
+
+puts "Let's talk about #{name}."
+puts "He's height is #{height * one_inch} inches tall."
+puts "He's weight is #{weight * one_pound } pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the tea ."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
