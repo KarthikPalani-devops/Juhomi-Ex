55365314375e2fec03510b2ecd89ce04e1a40cf2
KarthikPalani-devops <59925065+KarthikPalani-devops@users.noreply.github.com>
2021-03-20 23:55:12 +0530

    Add files via upload

---
+++ exp7.rb
@@ -0,0 +1,22 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}." # 'snow' will be printed as string directly.
+snow1 ="snow1"
+puts "its fleece was also white as #{snow1}"  # this works same as line 2
+puts "And everywhere that Mary went."
+puts "." * 10  # what'd that do?
+
+end1 = "C"    # assigning one string to end1
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6   # 'print' prints the value but does not move to new line
+puts end7 + end8 + end9 + end10 + end11 + end12   # + is used to cancate the values
