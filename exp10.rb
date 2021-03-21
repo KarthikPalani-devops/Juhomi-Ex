10e935787fd7781e9ff52775451770022058cfc0
KarthikPalani-devops <59925065+KarthikPalani-devops@users.noreply.github.com>
2021-03-21 22:13:49 +0530

    Add files via upload

---
+++ exp10.rb
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."   # \t appends a tab space
+persian_cat = "I'm split\non a line." # \n adds a new line hence string post \n is printed in new line
+backslash_cat = "I'm \\ a \\ cat."  # \\ acts a backspace
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+  """   # any number od lines inside  """ """ is printed
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
